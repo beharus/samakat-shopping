a2473ac87cfda0d9cb57dd1759b6fb612f5a22d6
unknown <sd979141@gmail.com>
2022-09-29 16:21:21 +0900

    header-card-active

--- js/script.js
+++ js/script.js
@@ -4,3 +4,25 @@
 
 
 //navbar-tagi-card-active
+            var elements = document.getElementsByClassName("items");
+            for(var i = 0; i < elements.length; i++)
+            {
+                elements[i].onclick = function(){
+                    
+                    // remove class from sibling
+                    
+                    var el = elements[0];
+                    while(el)
+                    {
+                        if(el.tagName === "DIV"){
+                            //remove class
+                            el.classList.remove("active");
+                            
+                        }
+                        // pass to the new sibling
+                        el = el.nextSibling;
+                    }
+                    
+                  this.classList.add("active");  
+                };
+            }
